c6cbb8764058c8af6400e5689fd5e2af8134ad1f
Razvan Predescu <rzvdaniel@gmail.com>
2022-01-24 21:55:33 +0000

    * Removed unnecessary StoredFlow * Simplified Flow repository

--- src/Conductor.Storage/Services/FlowRepository.cs
+++ src/Conductor.Storage/Services/FlowRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Conductor.Domain.Interfaces;
 using Conductor.Domain.Models;
-using Conductor.Storage.Models;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
@@ -13,7 +12,7 @@ namespace Conductor.Storage.Services
     public class FlowRepository : IFlowRepository
     {
         private readonly IMongoDatabase _database;
-        private IMongoCollection<StoredFlow> _collection => _database.GetCollection<StoredFlow>("Flows");
+        private IMongoCollection<Flow> _collection => _database.GetCollection<Flow>("Flows");
 
         static FlowRepository()
         {
@@ -22,31 +21,25 @@ namespace Conductor.Storage.Services
         public FlowRepository(IMongoDatabase database)
         {
             _database = database;
-            CreateIndexes(_collection);
         }
 
         public Flow Find(string flowId)
         {
-            var result = _collection.Find(x => x.ExternalId == flowId);
+            var result = _collection.Find(x => x.Id == flowId);
 
             if (!result.Any())
                 return null;
 
-            var json = result.First().Flow.ToJson();
-            var flow = JsonConvert.DeserializeObject<Flow>(json);
+            var flow = result.First();
            
             return flow;
         }
 
         public IEnumerable<Flow> GetAll()
         {
-            var results = _collection.AsQueryable().Select(x => x.Flow);
+            var results = _collection.AsQueryable();
 
-            foreach (var item in results)
-            {
-                var json = item.ToJson();
-                yield return JsonConvert.DeserializeObject<Flow>(json);
-            }
+            return results;
         }
 
         public IEnumerable<Flow> Get(int pageNumber, int pageSize)
@@ -58,12 +51,10 @@ namespace Conductor.Storage.Services
                 _collection
                     .AsQueryable()
                     .Skip((pageNumber - 1) * pageSize)
-                    .Take(pageSize)
-                    .Select(x => x.Flow) :
+                    .Take(pageSize) :
 
                 _collection
-                    .AsQueryable()
-                    .Select(x => x.Flow);
+                    .AsQueryable();
 
             foreach (var item in results)
             {
@@ -74,34 +65,13 @@ namespace Conductor.Storage.Services
 
         public void Save(Flow flow)
         {
-            var json = JsonConvert.SerializeObject(flow);
-            var doc = BsonDocument.Parse(json);
-
-            if (_collection.AsQueryable().Any(x => x.ExternalId == flow.Id))
+            if (_collection.AsQueryable().Any(x => x.Id == flow.Id))
             {
-                _collection.ReplaceOne(x => x.ExternalId == flow.Id, new StoredFlow()
-                {
-                    ExternalId = flow.Id,
-                    Flow = doc
-                });
+                _collection.FindOneAndReplace(x => x.Id == flow.Id, flow);
                 return;
             }
 
-            _collection.InsertOne(new StoredFlow()
-            {
-                ExternalId = flow.Id,
-                Flow = doc
-            });
-        }
-
-        static bool indexesCreated = false;
-        static void CreateIndexes(IMongoCollection<StoredFlow> collection)
-        {
-            if (!indexesCreated)
-            {
-                collection.Indexes.CreateOne(Builders<StoredFlow>.IndexKeys.Ascending(x => x.ExternalId), new CreateIndexOptions() { Background = true, Name = "idx_flow_id" });
-                indexesCreated = true;
-            }
+            _collection.InsertOne(flow);
         }
     }
 }
